03ccbbdbdb58d5a6b772e838d57c4b2b974a0433
Dan <dzhang@cs.umass.edu>
2017-12-02 22:28:06 -0500

    update script with more data

--- research/differential_privacy/multiple_teachers/run_stu.sh
+++ research/differential_privacy/multiple_teachers/run_stu.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 test_name='digit.test-MNIST-l2t' 
-sbatch -p  titanx-short --gres=gpu:2 --output=$test_name.out stu.sh digit -1 $test_name 
+test_names=('all-digits-MNIST-gammaC' 'all-digits-MNIST-gammaC-sh-asp' 'all-digits-MNIST-gammaC-sh' 'digit.test-MNIST')
+
+for t in "${test_names[@]}"
+do
+    sbatch -p  titanx-short --gres=gpu:2 --output=$t .out stu.sh digit -1 $t
+done
