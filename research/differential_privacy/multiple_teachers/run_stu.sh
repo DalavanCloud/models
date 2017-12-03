48ee84cb75f39c0f841189a45fee5ceb5d8ab891
Dan <dzhang@cs.umass.edu>
2017-12-02 22:30:36 -0500

    fix typo

--- research/differential_privacy/multiple_teachers/run_stu.sh
+++ research/differential_privacy/multiple_teachers/run_stu.sh
@@ -5,5 +5,5 @@ test_names=('all-digits-MNIST-gammaC' 'all-digits-MNIST-gammaC-sh-asp' 'all-digi
 
 for t in "${test_names[@]}"
 do
-    sbatch -p  titanx-short --gres=gpu:2 --output=$t .out stu.sh digit -1 $t
+    sbatch -p  titanx-short --gres=gpu:2 --output=$t.out stu.sh digit -1 $t
 done
